db5a3a616b9382dfe75783940f1602b8b83ffdda
RyomenDev <aakashvatsh007@gmail.com>
2025-02-02 11:46:47 +0530

    sample2-setup

--- sample1/client/src/App.jsx
+++ sample1/client/src/App.jsx
@@ -1,13 +1,13 @@
 // import VideoRecorder from "./VideoRecorder/VideoRecorder1.jsx";
-import VideoRecorder from "./VideoRecorder/VideoRecorder2.jsx";
+// import VideoRecorder from "./VideoRecorder/VideoRecorder2.jsx";
+import VideoRecorder from "./VideoRecorder/VideoRecorder3.jsx";
 
 function App() {
- 
   return (
     <>
       <VideoRecorder />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ sample2-Interview/client/src/App.jsx
@@ -0,0 +1,12 @@
+// import VideoRecorder from "./VideoRecorder/VideoRecorder1.jsx";
+
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
