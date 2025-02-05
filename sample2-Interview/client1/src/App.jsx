c5beeeef0a0c44541a6337ba101816b838b12722
RyomenDev <aakashvatsh007@gmail.com>
2025-02-05 15:41:13 +0530

    feat-client2

---
+++ sample2-Interview/client1/src/App.jsx
@@ -0,0 +1,16 @@
+// import VideoInterview from "./components/VideoInterview";
+// import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <>
+    <div className="bg-red-500">Hi</div>
+    {/* <Header/> */}
+    {/* <VideoInterview/> */}
+      {/* <VideoList/> */}
+    </>
+  );
+}
+
+export default App;
