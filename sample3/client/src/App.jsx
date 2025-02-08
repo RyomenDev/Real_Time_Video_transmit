6725ae063cb578293ea859df3720652d95751725
RyomenDev <aakashvatsh007@gmail.com>
2025-02-08 19:51:30 +0530

    sample-3

---
+++ sample3/client/src/App.jsx
@@ -0,0 +1,11 @@
+import VideoInterview from "./components/VideoInterview";
+
+const App = () => {
+  return (
+    <div className="bg-red-700">
+      <VideoInterview />
+    </div>
+  );
+};
+
+export default App;
